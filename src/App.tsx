9ea953944dc8ec9f785a2c23d832a4b181fd7104
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2025-08-28 21:39:26 -0300

    Starter React + Vite + Tailwind CSS
    starter proyect React + Vite + Tailwind 3.4.x

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind 3.4.14 ✅
+      </h1>
+    </div>
+  )
+}
+export default App
