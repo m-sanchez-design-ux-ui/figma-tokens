a3badbf213848ba399bb2f8d78c4365019f96f05
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2025-09-01 10:40:45 -0300

    Builder Tokens
    Config Builder Tokens in Tailwind css

---
+++ src/tokens/design-tokens.tsx
@@ -0,0 +1,20 @@
+import { Builder } from "@builder.io/react";
+
+Builder.register("editor.settings", {
+  styleStrictMode: true, // optional
+  designTokens: {
+    colors: [
+      { name: "Brand Red", value: "var(--red, #ff0000)" },
+      { name: "Brand Blue", value: "rgba(93, 150, 255, 1)" },
+    ],
+    spacing: [
+      { name: "Large", value: "var(--space-large, 20px)" },
+      { name: "Small", value: "var(--space-small, 10px)" },
+      { name: "Tiny", value: "5px" },
+    ],
+    fontFamily: [
+      { name: 'Serif Font', value: 'var(--serif-font, Times, serif)' },
+      { name: 'Primary Font', value: 'Roboto, sans-serif' },
+    ]
+  },
+});
