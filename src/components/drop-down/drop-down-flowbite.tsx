bb5ce626e7d4d6a80f19dcbfcbb0da04969abf68
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2025-09-12 09:42:02 -0300

    custom drop down flowbite
    custom drop down flowbite  with tokens figma

--- src/components/drop-down/drop-down-flowbite.tsx
+++ src/components/drop-down/drop-down-flowbite.tsx
@@ -2,32 +2,113 @@ import React from "react";
 import 'flowbite';
 
 export default function DropDownFlowbite() {
- 
+
   return (
     <React.Fragment>
-        
-        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-            Dropdown button 
-        </button>
+      <button 
+        id="dropdownDefaultButton" 
+        data-dropdown-toggle="dropdown" 
+        className="
+          text-text-contrast 
+          bg-brand-primary 
+          hover:bg-primary-600 
+          focus:ring-4 
+          focus:outline-none 
+          focus:ring-blue-300 
+          font-600 
+          rounded-button 
+          px-button-lg-x
+          py-button-lg-y
+          text-sm 
+          text-center 
+          inline-flex 
+          items-center 
+          min-h-11
+          dark:bg-blue-600 
+          dark:hover:bg-blue-700 
+          dark:focus:ring-blue-800" 
+        type="button"
+      >
+        Flowbite & Tokens
+      </button>
 
-        {/* Dropdown menu */}
-        <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-            </li>
-            </ul>
-        </div>
-
+      {/* Dropdown menu */}
+      <div 
+        id="dropdown" 
+        className="
+          z-10 
+          hidden 
+          bg-bg-drop-down 
+          divide-y 
+          divide-line-drop-down 
+          rounded-drop-down
+          shadow-sm 
+          w-44 
+          dark:bg-gray-700"
+      >
+        <ul className="
+          py-button-base-y text-center font-400 text-p text-text-paragraph cursor-pointer hover:text-text-secondary transition-colors" 
+          
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a 
+              href="#" 
+              className="
+                block 
+                px-button-base-y 
+                py-button-base-x 
+                hover:bg-gray-100 
+                dark:hover:bg-gray-600 
+                dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className="
+                block 
+                px-button-base-y 
+                py-button-base-x 
+                hover:bg-gray-100 
+                dark:hover:bg-gray-600 
+                dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className="
+                block 
+                px-button-base-y 
+                py-button-base-x 
+                hover:bg-gray-100 
+                dark:hover:bg-gray-600 
+                dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#" 
+              className="
+                block 
+                px-button-base-y 
+                py-button-base-x 
+                hover:bg-gray-100 
+                dark:hover:bg-gray-600 
+                dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </div>
     </React.Fragment>
   );
 }
