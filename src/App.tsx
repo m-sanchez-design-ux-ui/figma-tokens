e63069463d320bc44f045ca5d6f4599d486343b6
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2025-09-12 08:05:07 -0300

    Fix App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-// import Header from './components/header/header' 
-// import Footer from './components/footer/footer' 
-// import Card from './components/card/card'
-// import CardProfile from "./components/card/card-profile"
+import Header from './components/header/header' 
+import Footer from './components/footer/footer' 
+import Card from './components/card/card'
+import CardProfile from "./components/card/card-profile"
 
 function App() {
   return (
-<main className="bg-body flex items-center justify-center min-h-screen">
-    hola mundo
-{/* <div className="bg-white rounded-card p-card shadow-md">
-  <div className="w-full flex flex-col gap-gap-xs-2 bg-bg-primary-button">
-    <h1 className="text-title text-center">Figma Tokens Demo</h1>
-    <p className="text-paragraph text-center">¡Los estilos se están aplicando correctamente!</p>
-    
-    <div className="flex gap-lg justify-center">
-      <button className="bg-primary-button rounded-button py-button-base-y px-button-base-x font-bold text-white shadow-lg">
-        Botón Primario
-      </button>
-      <button className="bg-bg-card rounded-button py-button-lg-y px-button-lg-x font-medium text-text-primary border border-line-button-secondary">
-        Botón Secundario
-      </button>
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-between bg-bg-body">
+        
+        {/* Header Tokens*/}
+        <Header/>
 
-    <div className="mt-footer flex justify-center">
-      <div className="p-footer bg-bg-footer border-t border-line-footer">
-        <p className="text-sm text-text-tertiary">
-          Pie de página de ejemplo
-        </p>
-      </div>
-    </div>
-  </div>
-</div> */}
 
-</main>
+        {/* Container Title & Card */}
+        <section className='flex flex-col items-center gap-gap-lg'>
+
+          {/* Title h2 Tokens */}
+          <h2 className="text-text-title text-h2 font-600">
+            Here put the title...
+          </h2>
+
+          {/* Card Default Tokens */}  
+          <Card/>
+
+        </section>
+        
+          {/* Card Profile Tokens*/}  
+          <CardProfile/>
+
+        {/* Footer Tokens*/}
+        <Footer />
+    </main>
   )
 }
 export default App
